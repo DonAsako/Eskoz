6c46c0d13b97814fed4e110bd0ec4f4002f426ab
DonAsako <asakosan.pro@protonmail.com>
2025-05-19 01:39:29 +0200

    Add static

--- eskoz/settings.py
+++ eskoz/settings.py
@@ -132,7 +132,10 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR /  "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
