44e8d0f2b9b79d5bb255065daa695602a8d8899d
DonAsako <55788951+DonAsako@users.noreply.github.com>
2025-11-02 17:39:03 +0100

    refractor: admin

--- infosec/admin.py
+++ infosec/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.admin import PostAdmin
+
+from .models import (
+    CTF,
+    CVE,
+    Certification,
+    Issuer,
+    Writeup,
+)
+
+
+class WritupApdmin(PostAdmin):
+    fieldsets = PostAdmin.fieldsets + [
+        (
+            "CTF Information",
+            {
+                "fields": [
+                    ("ctf", "difficulty", "points", "solver_count"),
+                ]
+            },
+        ),
+    ]
+    list_display = ("ctf", "difficulty", "points")
+
+
+admin.site.register(Writeup, WritupApdmin)
+admin.site.register(CTF, admin.ModelAdmin)
+admin.site.register(Certification, admin.ModelAdmin)
+admin.site.register(Issuer, admin.ModelAdmin)
+admin.site.register(CVE, admin.ModelAdmin)
