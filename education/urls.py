0b50c6cd8402622831e749300813824169eb10aa
DonAsako <55788951+DonAsako@users.noreply.github.com>
2025-11-03 19:19:54 +0100

    feat: urls and views

--- education/urls.py
+++ education/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .views import course_list, course_detail
+from .views import course_list, module_list, lesson_list, lesson_detail
 
 app_name = "education"
 urlpatterns = [
     path("", course_list, name="course_list"),
-    path("<slug:slug>/", course_list, name="course_list"),
-    path("<slug:slug>/<slug:slug>/", course_detail, name="course_detail"),
+    path("<slug:slug_course>/", module_list, name="module_list"),
+    path("<slug:slug_course>/<slug:slug_module>/", lesson_list, name="lesson_list"),
+    path(
+        "<slug:slug_course>/<slug:slug_module>/<slug:slug_lesson>",
+        lesson_detail,
+        name="lesson_detail",
+    ),
 ]
--- education/views.py
+++ education/views.py
@@ -1,9 +1,37 @@
-from django.http import HttpResponse
+from .models import Course, Module, Lesson
+
+from django.shortcuts import render, get_object_or_404
 
 
-def course_list(request, slug=""):
-    return HttpResponse("Work in progress")
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, "education/course_list.html", {"courses": courses})
 
 
-def course_detail(request, slug=""):
-    return HttpResponse("Work in progress")
+def module_list(request, slug_course=""):
+    modules = Module.objects.all()
+    return render(request, "education/course_list.html", {"modules": modules})
+
+
+def lesson_list(request, slug_course="", slug_module=""):
+    course = get_object_or_404(Course, slug=slug_course)
+    module = get_object_or_404(Module, slug=slug_module, course=course)
+    lessons = Lesson.objects.filter(module=module)
+    return render(
+        request,
+        "education/lesson_list.html",
+        {"course": course, "module": module, "lessons": lessons},
+    )
+
+
+def lesson_detail(request, slug_course="", slug_module="", slug_lesson=""):
+    course = get_object_or_404(Course, slug=slug_course)
+    module = get_object_or_404(Module, slug=slug_module, course=course)
+
+    lesson = get_object_or_404(Lesson, slug=slug_lesson, module=module)
+
+    return render(
+        request,
+        "education/lesson_detail.html",
+        {"course": course, "module": module, "lesson": lesson},
+    )
