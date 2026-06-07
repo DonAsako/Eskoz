dd59a5de5d71b352ea1800f9396e59a8d5c6856e
DonAsako <55788951+DonAsako@users.noreply.github.com>
2026-06-07 04:15:32 +0200

    fix(security): sanitize rendered Markdown HTML to prevent stored XSS

--- apps/core/utils.py
+++ apps/core/utils.py
@@ -2,7 +2,82 @@ import os
 import uuid
 
 import markdown
+import nh3
 from django.utils.safestring import mark_safe
+
+_ALLOWED_TAGS = nh3.ALLOWED_TAGS | {
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "pre",
+    "details",
+    "summary",
+    "div",
+    "span",
+    "del",
+    "ins",
+    "sub",
+    "sup",
+    "kbd",
+    "mark",
+    "section",
+    "figure",
+    "figcaption",
+    "caption",
+    "table",
+    "thead",
+    "tbody",
+    "tfoot",
+    "tr",
+    "th",
+    "td",
+    "col",
+    "colgroup",
+    "dl",
+    "dt",
+    "dd",
+}
+
+_CLASS_ID_TAGS = {
+    "a",
+    "p",
+    "pre",
+    "code",
+    "span",
+    "div",
+    "details",
+    "summary",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "blockquote",
+    "table",
+    "thead",
+    "tbody",
+    "tr",
+    "th",
+    "td",
+    "ul",
+    "ol",
+    "li",
+    "section",
+}
+_ALLOWED_ATTRIBUTES = {tag: set(attrs) for tag, attrs in nh3.ALLOWED_ATTRIBUTES.items()}
+for _tag in _CLASS_ID_TAGS:
+    _ALLOWED_ATTRIBUTES.setdefault(_tag, set()).update({"class", "id"})
+_ALLOWED_ATTRIBUTES.setdefault("a", set()).update({"href", "title"})
+_ALLOWED_ATTRIBUTES.setdefault("img", set()).update({"src", "alt", "title"})
+_ALLOWED_ATTRIBUTES.setdefault("details", set()).add("open")
+_ALLOWED_ATTRIBUTES.setdefault("ol", set()).add("start")
+_ALLOWED_ATTRIBUTES.setdefault("td", set()).update({"colspan", "rowspan"})
+_ALLOWED_ATTRIBUTES.setdefault("th", set()).update({"colspan", "rowspan"})
+_ALLOWED_ATTRIBUTES.setdefault("abbr", set()).add("title")
 
 
 def upload_to_random_filename(instance, filename, folder):
@@ -88,4 +163,4 @@ def get_content_as_html(content):
             },
         },
     )
-    return mark_safe(html)
+    return mark_safe(nh3.clean(html, tags=_ALLOWED_TAGS, attributes=_ALLOWED_ATTRIBUTES))
